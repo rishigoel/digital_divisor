fbace2eac14cf6df78f320ad15c2e84eca057a40
Rishi Goel <rishigoel@Rishis-MacBook-Pro.local>
2018-02-27 21:06:27 -0500

    added in framework for reading photoresistors

--- digital_divisor/digital_divisor.ino
+++ digital_divisor/digital_divisor.ino
@@ -1,9 +1,28 @@
+// TODO change these:
+const int photoResistor1Pin = 1;
+const int photoResistor2Pin = 2;
+ 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(photoResistor1Pin, OUTPUT);
+  pinMode(photoResistor2Pin, OUTPUT);
 
+}
+
+int getResistanceDifference() {
+  int pr1 = analogRead(photoResistor1Pin);
+  int pr2 = analogRead(photoResistor2Pin);
+  int difference = pr2 - p1;
+  return difference;
+}
+
+// TODO: Map difference to positioning and operate motor
+void moveVisor(int difference) {
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  moveVisor(getResistanceDifference());
+  delay(100);
 }
