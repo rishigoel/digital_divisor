9db8a0888cf5a7ed284717a2e11acd84ff78bc82
Rishi Goel <rishigoel@v1020-wn-13-53.campus-dynamic.uwaterloo.ca>
2018-03-05 13:48:59 -0500

    added motor code. Just needs to be tested

--- digital_divisor/digital_divisor.ino
+++ digital_divisor/digital_divisor.ino
@@ -1,14 +1,24 @@
+#include <Adafruit_MotorShield.h>
+
 // TODO change these:
 const int photoResistor1Pin = 1;
 const int photoResistor2Pin = 2;
 const int limitSwitch1Pin = 3;
 const int limitSwitch2Pin = 4;
+const int rangeDiff = 500;
+const int motorSpeed = 150;
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
  
 void setup() {
   Serial.begin(9600);
   pinMode(photoResistor1Pin, OUTPUT);
   pinMode(photoResistor2Pin, OUTPUT);
-
+  AFMS.begin();  // create with the default frequency 1.6KHz -> change by passing in frequency as parameter in Hz
+  myMotor->setSpeed(motorSpeed);
 }
 
 int getResistanceDifference() {
@@ -20,11 +30,17 @@ int getResistanceDifference() {
 
 // TODO: Map difference to positioning and operate motor
 void moveVisor(int difference) {
-  
+  if (difference > rangeDiff) {
+    myMotor->run(FORWARD);
+  } else if (difference < rangeDiff) {
+    myMotor->run(BACKWARD);
+  } else {
+    stopMovingVisor();
+  }
 }
 
 void stopMovingVisor() {
-  
+  myMotor->run(RELEASE);
 }
 
 void loop() {
